95aecb83b7353b03fcc9b031ba11bff16feb60e2
Himanshu <singhhimanshu64179@gmail.com>
2021-08-24 07:48:13 +0530

    payment

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import Subtotal from './Subtotal';
 import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user}, dispatch] = useStateValue();
 
     return (
         <div className="checkout">
@@ -14,6 +14,7 @@ function Checkout() {
                 src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
                 />
                 <div>
+                    <h3>Hello, {user?.email}</h3>
                     <h2 className="checkout_title">Shopping Cart</h2>
                     {basket.map(item => (
                         <CheckoutProduct
